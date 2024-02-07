408d6d9b6ac7fb0784ea87fffacece36a2fb6053
Anissa Nishioka <ani.nishioka@gmail.com>
2024-02-07 14:35:21 -0800

    implement checkForWin

--- connect4.js
+++ connect4.js
@@ -91,7 +91,12 @@ function checkForWin() {
 
     // TODO: Check four cells to see if they're all legal & all color of current
     // player
+    for (let [y, x] of cells) {
+      if (y < 0 || y >= HEIGHT || x < 0 || x >= WIDTH) return false;
+      if (board[y][x] !== currPlayer) return false;
+    }
 
+    return true;
   }
 
   // using HEIGHT and WIDTH, generate "check list" of coordinates
@@ -105,9 +110,9 @@ function checkForWin() {
       // [ [y, x], [y, x], [y, x], [y, x] ]
 
       let horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
-      let vert;
-      let diagDL;
-      let diagDR;
+      let vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
+      let diagDL = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
+      let diagDR = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
 
       // find winner (only checking each win-possibility as needed)
       if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
